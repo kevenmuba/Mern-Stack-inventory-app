52735f661197bd067c52c025a924ee8a433cae49
kevenmuba <kevk0758@gmail.com>
2024-12-24 17:27:59 +0300

    finish authentication from the backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express";
 import dotenv from "dotenv";
 import { db } from "./db/db.js";
 
+import userRoutes from "./routes/User.routes.js";
+
+import { errorHandler, routeNotFound } from "./utils/errorHandler.js";
+
 dotenv.config();
 const app = express();
 console.log(process.env.MONGO_URI);
@@ -10,6 +14,14 @@ db();
 app.get("/", (req, res) => {
   res.send(`<h1>Welcome to the Inventory App</h1>`);
 });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/users", userRoutes);
+
+app.use("/*", routeNotFound);
+app.use(errorHandler);
 
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,7 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline ">INventory App</h1>;
+import "./App.css";
+
+function App() {
+  return <h1>Inventorie M SYS</h1>;
 }
+
+export default App;
